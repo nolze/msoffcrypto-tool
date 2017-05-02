3f7c6f3185ad4e93239611d060a162177ee309eb
T906518 <derek.armstrong@telus.com>
2017-05-02 09:57:47 -0600

    Updated for ascii password ability

--- ms_offcrypto_tool.py
+++ ms_offcrypto_tool.py
@@ -32,16 +32,39 @@ def generate_skey_from_privkey(privkey, encryptedKeyValue):
     skey = privkey.decrypt(encryptedKeyValue, None)
     return skey
 
+def generate_skey_from_password(password, saltValue, encryptedKeyValue, spinValue, blockkey)
+    # Initial round sha1(salt + password)
+    h = hashlib.sha1(saltValue.decode("base64") + password.encode("UTF-16LE"))
+
+    # Iteration of 0 -> spincount-1; hash = sha1(iterator + hash)
+    for i in range(0, spinValue, 1):
+        h = hashlib.sha1(struct.pack("<I", i) + h.digest())
+
+    h2 = hashlib.sha1(h.digest() + blockkey.decode("hex"))
+    # Needed to truncate skey to bitsize
+    a = h2.hexdigest()[:-8]
+    skey3 = a.decode("hex")
+
+    # AES encrypt the encryptedKeyValue with the skey and salt to get secret key
+    aes = AES.new(skey3, AES.MODE_CBC, salt.decode("base64"))
+    skey = aes.decrypt(encryptedKeyValue.decode("base64"))
+    return skey
+
 def parseinfo(ole):
     ole.seek(8)
     xml = parseString(ole.read())
     saltValue = xml.getElementsByTagName('keyData')[0].getAttribute('saltValue')
     saltValue = base64.b64decode(saltValue)
+    spinValue = xml.getElementsByTagNameNS("http://schemas.microsoft.com/office/2006/keyEncryptor/certificate", 'encryptedKey')[0].getAttribute('spinCount')
     encryptedKeyValue = xml.getElementsByTagNameNS("http://schemas.microsoft.com/office/2006/keyEncryptor/certificate", 'encryptedKey')[0].getAttribute('encryptedKeyValue')
     encryptedKeyValue = base64.b64decode(encryptedKeyValue)
+    blockkey = "146e0be7abacd0d6"
+    blockkey = blockkey.decode("hex")
     info = {
         'keyDataSalt': saltValue,
         'encryptedKeyValue': encryptedKeyValue
+        'blockkey': blockkey
+        'spinValue': spinValue
     }
     return info
 
@@ -53,6 +76,8 @@ class OfficeFile:
         self.secret_key = None
     def load_skey(self, secret_key):
         self.secret_key = secret_key
+    def load_password(self, private_key):
+        self.secret_key = generate_skey_from_password(self.info['password'], self.info['saltValue'], self.info['encryptedKeyValue'], self.info['spinValue'], self.info['blockkey'])
     def load_privkey(self, private_key):
         self.secret_key = generate_skey_from_privkey(private_key, self.info['encryptedKeyValue'])
     def decrypt(self, ofile):
@@ -64,6 +89,7 @@ def main():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-k', dest='secret_key', help='MS-OFFCRYPTO secretKey value (hex)')
     group.add_argument('-p', dest='private_key', type=argparse.FileType('rb'), help='RSA private key file')
+    group.add_argument('-P', dest='password', help='Password ASCII')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'))
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'))
     args = parser.parse_args()
@@ -77,6 +103,8 @@ def main():
         file.load_skey(binascii.unhexlify(args.secret_key))
     elif args.private_key:
         file.load_privkey(args.private_key)
+    elif args.password:
+        file.load_password(args.password)
 
     file.decrypt(args.outfile)
 
