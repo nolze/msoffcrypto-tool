22df686b4f3e327755bc11dc82461f086021470c
nolze <nolze@archlinux.us>
2018-05-13 02:39:37 +0900

    Remove key types other than password, renaming -P to -p

--- msoffcrypto/__main__.py
+++ msoffcrypto/__main__.py
@@ -1,28 +1,26 @@
-import sys
-import logging
+import logging, sys
+import argparse
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 import olefile
 
-from .msoffcrypto import OfficeFile
+from . import OfficeFile
 
 def ifWIN32SetBinary(io):
     if sys.platform == 'win32':
         import msvcrt, os
         msvcrt.setmode(io.fileno(), os.O_BINARY)
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-p', '--password', dest='password', help='Password text.')
+parser.add_argument('-v', dest='verbose', action='store_true', help='Print verbose information.')
+parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'), help='Input file.')
+parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'), help='Output file. If blank, stdout is used.')
+
 def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-k', dest='secret_key', help='MS-OFFCRYPTO secretKey value (hex)')
-    group.add_argument('-p', dest='private_key', type=argparse.FileType('rb'), help='RSA private key file')
-    group.add_argument('-P', dest='password', help='Password ASCII')
-    parser.add_argument('-v', dest='verbose', action='store_true', help='Print verbose information')
-    parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'))
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'))
     args = parser.parse_args()
 
     if not olefile.isOleFile(args.infile):
