a7c03fb4102e964997c9366d3298a995df2db826
nolze <nolze@archlinux.us>
2018-06-16 05:31:34 +0900

    Fix file name

--- tests/tests.sh
+++ tests/tests.sh
@@ -4,4 +4,4 @@ cd tests
 msoffcrypto-tool -p Password1234_ inputs/example_password.docx /tmp/example.docx
 diff /tmp/example.docx outputs/example.docx
 msoffcrypto-tool -p Password1234_ inputs/example_password.xlsx /tmp/example.xlsx
-diff /tmp/example.docx outputs/example.docx
+diff /tmp/example.xlsx outputs/example.xlsx
