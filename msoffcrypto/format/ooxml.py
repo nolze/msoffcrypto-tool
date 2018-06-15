e5c2bb6bf203034d05c36c63f29ca02ab44d872b
nolze <nolze@archlinux.us>
2018-06-15 22:26:44 +0900

    Refactor with flake8

--- msoffcrypto/format/ooxml.py
+++ msoffcrypto/format/ooxml.py
@@ -12,6 +12,7 @@ from ..method.ecma376_standard import ECMA376Standard
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+
 def _parse_encryptionheader(blob):
     flags, = unpack('<I', blob.read(4))
     # if mode == 'strict': compare values with spec.
@@ -36,6 +37,7 @@ def _parse_encryptionheader(blob):
     }
     return header
 
+
 def _parse_encryptionverifier(blob, algorithm):
     saltSize, = unpack('<I', blob.read(4))
     salt = blob.read(16)
@@ -54,6 +56,7 @@ def _parse_encryptionverifier(blob, algorithm):
     }
     return verifier
 
+
 def _parseinfo_standard(ole):
     headerFlags, = unpack('<I', ole.read(4))
     encryptionHeaderSize, = unpack('<I', ole.read(4))
@@ -64,15 +67,16 @@ def _parseinfo_standard(ole):
     blob = io.BytesIO(block)
     algIdMap = {
         0x0000660E: 'AES-128',
-        0x0000660F: 'AES-192',   
-        0x00006610: 'AES-256',     
+        0x0000660F: 'AES-192',
+        0x00006610: 'AES-256',
     }
-    verifier = _parse_encryptionverifier(blob, "AES" if header['algId'] & 0xFF00 == 0x6600 else "RC4") ## TODO: Fix
+    verifier = _parse_encryptionverifier(blob, "AES" if header['algId'] & 0xFF00 == 0x6600 else "RC4")  # TODO: Fix
     info = {
         'header': header,
         'verifier': verifier,
     }
     return info
+
 
 def _parseinfo_agile(ole):
     ole.seek(8)
@@ -96,15 +100,16 @@ def _parseinfo_agile(ole):
     }
     return info
 
+
 def _parseinfo(ole):
     versionMajor, versionMinor = unpack('<HH', ole.read(4))
-    if versionMajor == 4 and versionMinor == 4: # Agile
+    if versionMajor == 4 and versionMinor == 4:  # Agile
         return 'agile', _parseinfo_agile(ole)
-    elif versionMajor in [2, 3, 4] and versionMinor == 2: # Standard
+    elif versionMajor in [2, 3, 4] and versionMinor == 2:  # Standard
         return 'standard', _parseinfo_standard(ole)
-        # raise AssertionError("Unsupported EncryptionInfo version (Standard Encryption)")
-    elif versionMajor in [3, 4] and versionMinor == 3: # Extensible
+    elif versionMajor in [3, 4] and versionMinor == 3:  # Extensible
         raise AssertionError("Unsupported EncryptionInfo version (Extensible Encryption)")
+
 
 class OOXMLFile(base.BaseOfficeFile):
     def __init__(self, file):
@@ -114,7 +119,7 @@ class OOXMLFile(base.BaseOfficeFile):
         self.type, self.info = _parseinfo(self.file.openstream('EncryptionInfo'))
         self.secret_key = None
         if self.type == 'agile':
-            ## TODO: Support aliases?
+            # TODO: Support aliases?
             self.keyTypes = ('password', 'private_key', 'secret_key')
         elif self.type == 'standard':
             self.keyTypes = ('password', 'secret_key')
@@ -124,7 +129,14 @@ class OOXMLFile(base.BaseOfficeFile):
     def load_key(self, password=None, private_key=None, secret_key=None):
         if password:
             if self.type == 'agile':
-                self.secret_key = ECMA376Agile.makekey_from_password(password, self.info['passwordSalt'], self.info['passwordHashAlgorithm'], self.info['encryptedKeyValue'], self.info['spinValue'], self.info['passwordKeyBits'])
+                self.secret_key = ECMA376Agile.makekey_from_password(
+                    password,
+                    self.info['passwordSalt'],
+                    self.info['passwordHashAlgorithm'],
+                    self.info['encryptedKeyValue'],
+                    self.info['spinValue'],
+                    self.info['passwordKeyBits']
+                )
             elif self.type == 'standard':
                 self.secret_key = ECMA376Standard.makekey_from_password(
                     password,
@@ -144,18 +156,22 @@ class OOXMLFile(base.BaseOfficeFile):
 
     def decrypt(self, ofile):
         if self.type == 'agile':
-            obuf = ECMA376Agile.decrypt(self.secret_key, self.info['keyDataSalt'], self.info['keyDataHashAlgorithm'], self.file.openstream('EncryptedPackage'))
+            obuf = ECMA376Agile.decrypt(
+                self.secret_key, self.info['keyDataSalt'],
+                self.info['keyDataHashAlgorithm'],
+                self.file.openstream('EncryptedPackage')
+            )
             ofile.write(obuf)
         elif self.type == 'standard':
             obuf = ECMA376Standard.decrypt(self.secret_key, self.file.openstream('EncryptedPackage'))
             ofile.write(obuf)
 
-    ## For backward compatibility; Should be removed in 4.0
+    # For backward compatibility; Should be removed in 4.0
     def load_password(self, password):
         self.load_key(password=password)
-    
+
     def load_privkey(self, privkey):
         self.load_key(private_key=privkey)
-    
+
     def load_skey(self, skey):
         self.load_key(secret_key=skey)
--- msoffcrypto/method/ecma376_standard.py
+++ msoffcrypto/method/ecma376_standard.py
@@ -9,27 +9,27 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+
 class ECMA376Standard:
     def __init__(self):
         pass
-    
+
     @staticmethod
     def decrypt(key, ibuf):
         obuf = io.BytesIO()
         totalSize = unpack('<I', ibuf.read(4))[0]
         logger.debug("totalSize: {}".format(totalSize))
-        remaining = totalSize
         ibuf.seek(8)
         aes = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
         decryptor = aes.decryptor()
         x = ibuf.read()
         dec = decryptor.update(x) + decryptor.finalize()
         obuf.write(dec[:totalSize])
-        return obuf.getvalue() # return obuf.getbuffer()
-    
+        return obuf.getvalue()  # return obuf.getbuffer()
+
     @staticmethod
     def verifykey(key, encryptedVerifier, encryptedVerifierHash):
-        ## https://msdn.microsoft.com/en-us/library/dd926426(v=office.12).aspx
+        # https://msdn.microsoft.com/en-us/library/dd926426(v=office.12).aspx
         aes = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
         decryptor = aes.decryptor()
         verifier = decryptor.update(encryptedVerifier)
@@ -38,14 +38,14 @@ class ECMA376Standard:
         verifierHash = decryptor.update(encryptedVerifierHash)[:sha1().digest_size]
         logging.debug([hash, verifierHash])
         return hash == verifierHash
-    
+
     @staticmethod
     def makekey_from_password(password, algId, algIdHash, providerType, keySize, saltSize, salt):
         xor_bytes = lambda a, b: bytearray([p ^ q for p, q in zip(a, b)])
-        
-        ## https://msdn.microsoft.com/en-us/library/dd925430(v=office.12).aspx
+
+        # https://msdn.microsoft.com/en-us/library/dd925430(v=office.12).aspx
         ITER_COUNT = 50000
-        
+
         password = password.encode("UTF-16LE")
         h = sha1(salt + password).digest()
         for i in range(ITER_COUNT):
@@ -61,7 +61,7 @@ class ECMA376Standard:
         x1 = sha1(buf1).digest()
         buf2 = b"\x5c" * 64
         buf2 = xor_bytes(hfinal, buf2[:cbHash]) + buf2[cbHash:]
-        x2 = sha1(buf2).digest() ## In spec but unused
+        x2 = sha1(buf2).digest()  # In spec but unused
         x3 = x1 + x2
         keyDerived = x3[:cbRequiredKeyLength]
         logger.debug(keyDerived)
