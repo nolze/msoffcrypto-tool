e7f02c0c7f9c33814ff40332a169ecc3f50cf525
nolze <nolze@archlinux.us>
2018-06-16 21:49:25 +0900

    Add test

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,3 +5,5 @@ msoffcrypto-tool -p Password1234_ inputs/example_password.docx /tmp/example.docx
 diff /tmp/example.docx outputs/example.docx
 msoffcrypto-tool -p Password1234_ inputs/example_password.xlsx /tmp/example.xlsx
 diff /tmp/example.xlsx outputs/example.xlsx
+msoffcrypto-tool -p Password1234_ inputs/ecma376standard_password.docx /tmp/ecma376standard_password_plain.docx
+diff /tmp/ecma376standard_password_plain.docx outputs/ecma376standard_password_plain.docx
