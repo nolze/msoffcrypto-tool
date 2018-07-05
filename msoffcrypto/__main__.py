dafc91f093e2c35951abba56e7fe114878288dbe
nolze <nolze@archlinux.us>
2018-07-05 13:26:06 +0900

    Refactor

--- msoffcrypto/__main__.py
+++ msoffcrypto/__main__.py
@@ -19,7 +19,7 @@ def ifWIN32SetBinary(io):
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-p', '--password', dest='password', help='Password text.')
-group.add_argument('-e', '--protected', dest='protected', action='store_true', help='Check if the file is encrypted. If called, all other arguments, bar input, are ignored')
+group.add_argument('-e', '--protected', dest='protected', action='store_true', help='Check if the file is encrypted. If called, all other arguments, bar input, are ignored.')
 parser.add_argument('-v', dest='verbose', action='store_true', help='Print verbose information.')
 parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'), help='Input file.')
 parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'), help='Output file. If blank, stdout is used.')
@@ -35,17 +35,15 @@ def main():
         logger.removeHandler(logging.NullHandler())
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
-    if args.protected:
-        file = OfficeFile(args.infile)
-        if (file.keyTypes == ['password'] and not file.info.fib.base.fEncrypted):
-            print(0)
-            return 0
-        else:
-            print(1)
-            return 1
-    else:
-        file = OfficeFile(args.infile)
+    file = OfficeFile(args.infile)
 
+    if args.protected:
+        if file.format == 'doc97' and not file.info.fib.base.fEncrypted:
+            print("{}: not encrypted".format(args.infile.name), file=sys.stderr)
+            sys.exit(1)
+        else:
+            logger.debug("{}: encrypted".format(args.infile.name))
+    else:
         if args.password:
             # this will always raise an error for 2000-03 files, cannot be decrypted.
             # TODO: check and return output stating such, allowing safedocs to ignore file.
